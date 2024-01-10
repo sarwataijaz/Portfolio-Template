93ba3fbf3dfc2acf62edede283f36041f0ead05f
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-01-10 23:20:43 +0500

    Updated website

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/mountains.jpg";
+import image from "../images/lake.jpg";
 
 const imageAltText = "Sarwat Aijaz - Aspiring Android App Developer and Content Writer";
 
