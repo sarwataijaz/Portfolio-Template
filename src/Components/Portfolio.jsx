51c021a877d51f3812c943f3ec7c9358ab008a64
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-01-10 23:03:20 +0500

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/table.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "table with a laptop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My first Android App",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This app features a single activity for efficient GPA calculation, allowing users to select credit hours and grades.",
+    url: "https://github.com/sarwataijaz/GPA-Calculator.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Mastering Java for Beginners",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      "All concepts including Object Oriented Programming and GUI are included along with comments for explanation.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "My Resume Site",
+    title: "Mastering Kotlin for Android Dev",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "All stuff about Kotlin that are needed to be known for starting Android Development (if you are from C++/Java Background)",
+    url: "https://github.com/sarwataijaz/Kotlin-for-Android.git",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Tic-Tac-Toe Game",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A stylish beginner-friendly tictactoe adventure using JavaFX.",
+    url: "https://github.com/sarwataijaz/Tic-Tac-Toe-Project.git",
   },
 ];
 
